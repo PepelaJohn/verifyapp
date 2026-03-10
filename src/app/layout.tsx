ba7e9b46a6c906246cba717b2c3c9b560448d8a2
John Pepela <pepelajahy@gmail.com>
2026-03-11 00:01:14 +0300

    Add @vercel/analytics dependency and integrate analytics component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata: Metadata = {
   title: "License Admin",
   description: "Chatbot License Management",
@@ -9,7 +9,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <>
+        {children}
+        <Analytics/>
+        </>
+        </body>
+        
     </html>
   );
 }
